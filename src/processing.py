0df5a520f392aea249a2e92323b201741ca5a987
Almaz <almaz21214m@gmail.com>
2025-05-09 15:12:37 +0500

    checked linters

--- src/processing.py
+++ src/processing.py
@@ -1,30 +1,14 @@
 from datetime import datetime
 
 
-def filter_by_state(data: list[dict], state: str='EXECUTED') -> list[dict]:
+def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """Фильтр для списка словарей."""
     return [item for item in data if item.get('state') == state]
 
 
-def sort_by_date(data: list[dict], descending: bool=True) -> list[dict]:
+def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
     """Сортировка по дате."""
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
-
-# Пример использования
-data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-filtered_data = filter_by_state(data)
-print(filtered_data)  # Вывод: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-
-
-
-
-sorted_data = sort_by_date(data)
-print(sorted_data)
+    return sorted(
+        data,
+        key=lambda x: datetime.fromisoformat(x['date']), reverse=descending
+    )
